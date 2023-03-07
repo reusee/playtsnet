4c50295f493c2628d336a52af6d69ecdfc1ba6c0
reus <reusee@gmail.com>
2023-03-07 18:04:19 +0800

    update

--- main.go
+++ main.go
@@ -1,10 +1,26 @@
 package main
 
 import (
+	"context"
+
+	"tailscale.com/client/tailscale"
 	"tailscale.com/tsnet"
 )
+
+func init() {
+	tailscale.I_Acknowledge_This_API_Is_Unstable = true
+}
 
 func main() {
 	server := &tsnet.Server{}
 	ce(server.Start())
+
+	ctx := context.Background()
+
+	conn, err := server.Dial(ctx, "tcp", "100.79.235.80:10000")
+	ce(err)
+	defer conn.Close()
+
+	select {}
+
 }
